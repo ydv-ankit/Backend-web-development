bb34205cd151228a1834f67afb53ab62df444268
Ankit Ydv <innocentankit1141@gmail.com>
2023-03-18 22:48:55 +0530

    mounting routers

--- methods.js
+++ methods.js
@@ -3,23 +3,54 @@ const app = express()
 // middleware functions, change post data into json format
 app.use(express.json())
 
-let users = {};
+const userRouter = express.Router();
+app.use("/user", userRouter)
 
-app.get('/users', (req, res)=>{
-    res.send(users);
+// miniapp
+userRouter
+.route("/")
+.get(getUser)
+.post(postUser)
+.patch(updateUser)
+.delete(deleteUser);
+
+userRouter.route("/:id").get(getUserById)
+
+let users = [
+    {
+        'id': 1,
+        'name': "Ankit"
+    },
+    {
+        "id": 2,
+        "name": "Cyfer"
+    },
+    {
+        "id": 3,
+        "name": "hacker"
+    }
+];
+
+
+// start server to listen
+app.listen(8080, ()=>{ 
+    console.log("server live at http://127.0.0.1:8080")
 })
 
-app.post('/users', (req, res)=>{
-    // console.log(req.body);
+function getUser(req, res){
+    console.log(req.query)  // query
+    res.json(users);
+}
+
+function postUser(req, res){
     users = req.body;
     res.json({
         message: "data recieved successfully",
         user: req.body
     })
-})
+}
 
-// patch -> update
-app.patch('/users', (req, res)=>{
+function updateUser(req, res){
     console.log("req.body -> ", req.body)
     let dataToBeUpdated = req.body;
     for(key in dataToBeUpdated){
@@ -28,17 +59,26 @@ app.patch('/users', (req, res)=>{
     res.json({
         message: "data updated"
     })
-})
+}
 
-// delete methods -> to delete data
-app.delete('/users', (req, res)=>{
+function deleteUser(req, res){
     users = {}
     res.json({
         message: "data has been deleted"
     })
-})
+}
 
-// start server to listen
-app.listen(8080, ()=>{ 
-    console.log("server live at http://127.0.0.1:8080")
-})
+function getUserById(req, res){
+    console.log(req.params.id);
+    let paramId = req.params.id;
+    let obj= {};
+    for(let i = 0; i < users.length; i++){
+        if(users[i].id == paramId){
+            obj = users[i]
+        }
+    }
+    res.json({
+        message:"request recieved",
+        data:obj
+    })
+}
