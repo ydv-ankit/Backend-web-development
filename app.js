1aa056e58699e0ce6df87e5ccfe27d5a1de00de7
Ankit Ydv <innocentankit1141@gmail.com>
2023-03-18 14:49:11 +0530

    404 added

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express =  require('express')
 const app = express()
 
 app.get("/", (req, res)=>{
-    res.send("Welcome to my server app")
+    res.sendFile("./views/index.html", {root : __dirname})
 })
 
+app.get("/about", (req, res)=>{
+    res.sendFile("./views/about.html", {root : __dirname})
+})
+
+// redirects
+app.get("/about-us", (req, res)=>{
+    res.redirect("/about")
+})
+
+// 404 page
+app.use((req, res)=>{
+    res.status(404).sendFile("./views/404.html", {root : __dirname})
+})
+
+// start server to listen
 app.listen(8080, ()=>{ 
     console.log("server live at http://127.0.0.1:8080")
 })
